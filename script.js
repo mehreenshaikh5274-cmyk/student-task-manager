7d7ed647ded9d3177605e25f7c4c4cbd38d42c5d
mehreenshaikh5274-cmyk <mehreenshaikh5274@gmail.com>
2026-09-23 22:09:41 +0530

    Initial Student Task Manager

---
+++ script.js
@@ -0,0 +1,18 @@
+function addTask() {
+
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value;
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.textContent = taskText;
+
+    document.getElementById("taskList").appendChild(li);
+
+    taskInput.value = "";
+}
